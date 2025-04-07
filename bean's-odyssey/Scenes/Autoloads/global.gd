9710fb56f71dc6824481cad87845a2a6bdfb5e37
Logan Howarth <loganhowarth@icloud.com>
2025-04-07 10:07:39 -0600

    Delete bean's-odyssey directory

--- bean's-odyssey/Scenes/Autoloads/global.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-var game: Node
-var transition_layer: TransitionLayer
-var debug_menu: DebugMenu
-var room_manager: RoomManager
-
-var debug_enabled = false
-var can_pause = false
--- bean's-odyssey/Scenes/Objects/obama.gd
+++
@@ -1,6 +0,0 @@
-extends Sprite2D
-
-var change = randf_range(-100, 100)
-
-func _process(delta: float) -> void:
-	rotation_degrees += change * delta
